0d2db409a6a1e364c421470d8f88f336e2bafbfa
Jerome Brette <jb447c@att.com>
2019-02-27 11:10:07 -0600

    Consolidate mariadb helm-operator POC makefile

--- mariadb-operator/deleteIt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl delete -f deploy/crds/openstackhelm_v1alpha1_mariadb_cr.yaml -n operatorpoc
-kubectl delete -f deploy/operator.yaml -n operatorpoc
-kubectl delete -f deploy/role_binding.yaml -n operatorpoc
-kubectl delete -f deploy/role.yaml -n operatorpoc
-kubectl delete -f deploy/service_account.yaml -n operatorpoc
-kubectl delete -f deploy/crds/openstackhelm_v1alpha1_mariadb_crd.yaml -n operatorpoc
-kubectl delete namespace operatorpoc
