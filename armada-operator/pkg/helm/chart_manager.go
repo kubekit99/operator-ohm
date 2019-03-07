97de70c94b18f5a24fcd0f445b679ba931cebc46
Ian Howell <ian.howell0@gmail.com>
2019-03-07 10:37:37 -0600

    Add method for getting chart from external sources
    This adds the ability to download and untar a tarball containing a
    chart. It also adds the skeleton code for cloning a git repo containing
    a chart.

--- armada-operator/pkg/helm/chart_manager.go
+++ armada-operator/pkg/helm/chart_manager.go
@@ -15,8 +15,16 @@
 package helm
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 
 	av1 "github.com/kubekit99/operator-ohm/armada-operator/pkg/apis/armada/v1alpha1"
@@ -35,7 +43,7 @@ import (
 type chartmanager struct {
 	storageBackend   *storage.Storage
 	tillerKubeClient *kube.Client
-	chartDir         string
+	chartLocation    *av1.ArmadaChartSource
 
 	tiller      *tiller.ReleaseServer
 	releaseName string
@@ -90,6 +98,7 @@ func (m *chartmanager) Sync(ctx context.Context) error {
 	}
 
 	// Load the chart and config based on the current state of the custom resource.
+	// Replace this with sources from armada
 	chart, config, err := m.loadChartAndConfig()
 	if err != nil {
 		return fmt.Errorf("failed to load chart and config: %s", err)
@@ -151,8 +160,8 @@ func (m chartmanager) syncReleaseStatus(status av1.ArmadaChartStatus) error {
 
 func (m chartmanager) loadChartAndConfig() (*cpb.Chart, *cpb.Config, error) {
 	// chart is mutated by the call to processRequirements,
-	// so we need to reload it from disk every time.
-	chart, err := chartutil.LoadDir(m.chartDir)
+	// so we need to reload it every time.
+	chart, err := m.getChart()
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to load chart: %s", err)
 	}
@@ -219,4 +228,155 @@ func (m chartmanager) ReconcileRelease(ctx context.Context) (*rpb.Release, error
 // UninstallRelease performs a Helm release uninstall.
 func (m chartmanager) UninstallRelease(ctx context.Context) (*rpb.Release, error) {
 	return uninstallRelease(ctx, m.storageBackend, m.tiller, m.releaseName)
+}
+
+func (m chartmanager) getChart() (*cpb.Chart, error) {
+	var pathToChart string
+	var err error
+	switch m.chartLocation.Type {
+	case "git":
+		pathToChart, err = m.gitClone()
+	case "tar":
+		pathToChart, err = m.getTarball()
+	case "local":
+		pathToChart = m.chartLocation.Location
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	chart, err := chartutil.LoadDir(pathToChart)
+	if err != nil {
+		return nil, err
+	}
+
+	err = sourceCleanup(pathToChart)
+	if err != nil {
+		// TODO: log a warning
+	}
+
+	return chart, nil
+}
+
+func (m *chartmanager) gitClone() (string, error) {
+	// TODO(Ian): Finish this method
+	repoURL := m.chartLocation.Location
+	if repoURL == "" {
+		return "", errors.New("Must provide a git url")
+	}
+	return "", nil
+}
+
+func (m *chartmanager) getTarball() (string, error) {
+	tarballPath, err := m.downloadTarball(false)
+	if err != nil {
+		return "", err
+	}
+	return extractTarball(tarballPath)
+}
+
+// downloadTarball Downloads a tarball to /tmp and returns the path
+func (m *chartmanager) downloadTarball(verify bool) (string, error) {
+	file, err := ioutil.TempFile("", "armada")
+	if err != nil {
+		return "", err
+	}
+	response, err := http.Get(m.chartLocation.Location)
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	file.Write(body)
+
+	return file.Name(), nil
+}
+
+// extractTarball Extracts a tarball to /tmp and returns the path
+func extractTarball(tarballPath string) (string, error) {
+	if _, err := os.Stat(tarballPath); err != nil {
+		if os.IsNotExist(err) {
+			return "", fmt.Errorf("%s does not exist", tarballPath)
+		}
+		return "", err
+	}
+
+	tempDir, err := ioutil.TempDir("", "armada")
+	if err != nil {
+		return "", err
+	}
+
+	fileContents, err := os.Open(tarballPath)
+	if err != nil {
+		return "", err
+	}
+
+	gzr, err := gzip.NewReader(fileContents)
+	if err != nil {
+		return "", err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	done := false
+	for !done {
+		if err := readFromArchive(tr, tempDir); err != nil {
+			if err != io.EOF {
+				return "", err
+			}
+			// io.EOF means there's no more data to be read
+			done = true
+		}
+	}
+	return tempDir, nil
+}
+
+// readFromArchive reads a an item from tr, saves it to dir, then move tr to the next item
+func readFromArchive(tr *tar.Reader, dir string) error {
+	header, err := tr.Next()
+	if err != nil {
+		// This catches EOF, which means that we're done
+		return err
+	}
+
+	if header == nil {
+		// if the header is nil, just skip it (not sure how this happens)
+		return nil
+	}
+
+	target := filepath.Join(dir, header.Name)
+
+	switch header.Typeflag {
+	case tar.TypeDir:
+		if _, err := os.Stat(target); err != nil {
+			if err := os.MkdirAll(target, 0755); err != nil {
+				return err
+			}
+		}
+	case tar.TypeReg:
+		f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+		if err != nil {
+			return err
+		}
+
+		// copy over contents
+		if _, err := io.Copy(f, tr); err != nil {
+			return err
+		}
+
+		// manually close here after each file operation; defering would cause each file close
+		// to wait until all operations have completed.
+		f.Close()
+	}
+	return nil
+}
+
+func sourceCleanup(path string) error {
+	// TODO(Ian): Finish this method
+	return nil
 }
